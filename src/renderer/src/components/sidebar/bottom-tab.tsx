a22076174ec19f82bb456ea8070a37312744128c
asli-nobita <hamzanajfi2765@gmail.com>
2025-03-22 17:59:08 +0530

    frontend

--- src/renderer/src/components/sidebar/bottom-tab.tsx
+++ src/renderer/src/components/sidebar/bottom-tab.tsx
@@ -1,37 +1,36 @@
 /* eslint-disable */
-import { Tabs } from '@chakra-ui/react'
-import { FiCamera, FiMonitor } from 'react-icons/fi'
-import { sidebarStyles } from './sidebar-styles'
-import CameraPanel from './camera-panel'
-import ScreenPanel from './screen-panel'
+import { Tabs } from "@chakra-ui/react";
+import { FiCamera, FiMonitor } from "react-icons/fi";
+import { sidebarStyles } from "./sidebar-styles";
+import CameraPanel from "./camera-panel";
+import ScreenPanel from "./screen-panel";
 
 function BottomTab(): JSX.Element {
-  return (
-    <Tabs.Root 
-      defaultValue="camera" 
-      variant="plain"
-      {...sidebarStyles.bottomTab.container}
-    >
-      <Tabs.List {...sidebarStyles.bottomTab.list}>
-        <Tabs.Trigger value="camera" {...sidebarStyles.bottomTab.trigger}>
-          <FiCamera />
-          Camera
-        </Tabs.Trigger>
-        <Tabs.Trigger value="screen" {...sidebarStyles.bottomTab.trigger}>
-          <FiMonitor />
-          Screen
-        </Tabs.Trigger>
-      </Tabs.List>
-
-      <Tabs.Content value="camera">
-        <CameraPanel />
-      </Tabs.Content>
-      
-      <Tabs.Content value="screen">
-        <ScreenPanel />
-      </Tabs.Content>
-    </Tabs.Root>
-  );
+	// Return an empty div instead of the tabs
+	// This preserves the component in the DOM structure but makes it invisible
+	return (
+		<div style={{ display: "none" }}>
+			{/* Original tabs component is hidden */}
+			<Tabs.Root defaultValue="camera" variant="plain" {...sidebarStyles.bottomTab.container}>
+				<Tabs.List {...sidebarStyles.bottomTab.list}>
+					<Tabs.Trigger value="camera" {...sidebarStyles.bottomTab.trigger}>
+						<FiCamera />
+						Camera
+					</Tabs.Trigger>
+					<Tabs.Trigger value="screen" {...sidebarStyles.bottomTab.trigger}>
+						<FiMonitor />
+						Screen
+					</Tabs.Trigger>
+				</Tabs.List>
+				<Tabs.Content value="camera">
+					<CameraPanel />
+				</Tabs.Content>
+				<Tabs.Content value="screen">
+					<ScreenPanel />
+				</Tabs.Content>
+			</Tabs.Root>
+		</div>
+	);
 }
 
-export default BottomTab
+export default BottomTab;
